fe01ec83701de3cd1b7c15d0c00a58d291f6b9b1
MostafaAlii <mostafa0alii@gmail.com>
2023-08-28 18:17:14 +0300

    starting with AccessTokenService two

--- services/identity/app/Services/Api/AccessTokenService.php
+++ services/identity/app/Services/Api/AccessTokenService.php
@@ -1,8 +1,12 @@
 <?php
 declare(strict_types=1);
 namespace App\Services\Api;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+
 final class AccessTokenService {
     public function create(Model|User $user): string {
-        
+        $token = Str::random(64);
     }
 }
